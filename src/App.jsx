c3f6a990d447471d746ad5fb3bbec05f7134e1e6
kasapovicrijad <rikacblyat@gmail.com>
2023-01-29 20:17:48 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Home from "./components/Home/Home";
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+
+
+
+
+function App() {
+  return (
+    <div>
+    <Home></Home>
+    <Navbar></Navbar>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import './navbar.css'
+import Profile from '../images/profile.png';
+
+
+const Navbar = () => {
+    return(
+        
+        <div className='nav'>
+            <div className="right-links">
+                <div className="nav-links"><a href="#">Gmail</a></div>
+                <div className="nav-links"><a href="#">Images</a></div>
+                <div className="nav-links">
+                <svg class="gb_Ze" focusable="false" viewBox="0 0 24 24"><path d="M6,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM16,6c0,1.1 0.9,2 2,2s2,-0.9 2,-2 -0.9,-2 -2,-2 -2,0.9 -2,2zM12,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2z"></path></svg>  
+                </div>
+                <div className="icon"><a href="#"><img src={Profile} alt="img"/></a></div>
+            </div>
+            
+        </div>
+        
+    )
+}
+
+export default Navbar;
