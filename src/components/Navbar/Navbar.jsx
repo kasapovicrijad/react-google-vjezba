d5876e1744d4567c5c2dc0014728770642c0de31
kasapovicrijad <rikacblyat@gmail.com>
2023-01-29 20:58:52 +0100

    neke izmjene ono na

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import './navbar.css'
 import Profile from '../images/profile.png';
+import Tools from '../images/tools.png';
 
 
 const Navbar = () => {
@@ -11,7 +12,7 @@ const Navbar = () => {
                 <div className="nav-links"><a href="#">Gmail</a></div>
                 <div className="nav-links"><a href="#">Images</a></div>
                 <div className="nav-links">
-                <svg class="gb_Ze" focusable="false" viewBox="0 0 24 24"><path d="M6,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM16,6c0,1.1 0.9,2 2,2s2,-0.9 2,-2 -0.9,-2 -2,-2 -2,0.9 -2,2zM12,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2z"></path></svg>  
+                <img src={Tools} alt="tools" />
                 </div>
                 <div className="icon"><a href="#"><img src={Profile} alt="img"/></a></div>
             </div>
